022af72554b03ae46a6091c46a50d44263e82c7f
Nir Ben-Rei <nir.ben.rei@sap.com>
2021-02-04 17:01:43 +0200

    pr-1

--- CollidingFeatures.java
+++ CollidingFeatures.java
@@ -2,4 +2,8 @@ class CollidingFeatures {
     public boolean isContainingChar(String str){
         return "abc".contains(str);
     }
+
+    public boolean isFeatureAOn(){
+        return isContainingChar("a");
+    }
 }
